5e8d07f9a80ced80e4bd90d45338b675965fb68b
SwaTik91 <shalmiev.s@mail.ru>
2025-10-01 03:56:09 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,8 @@
 
 self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open('mishpucha-v1').then(cache => cache.addAll([
+  e.waitUntil(caches.open('mishpucha-v2').then(cache => cache.addAll([
     './','./index.html','./manifest.webmanifest',
-    './assets/css/style.css',
-    './assets/js/app.js','./assets/js/ui.js','./assets/js/data.js',
-    './assets/js/feed.js','./assets/js/tree.js','./assets/js/calendar.js',
-    './assets/js/groups.js','./assets/js/events.js','./assets/js/synagogues.js',
-    './assets/js/admin.js','./assets/js/profile.js'
+    './assets/css/style.css'
   ])));
 });
 self.addEventListener('fetch', (e)=>{
